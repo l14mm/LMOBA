da06541f211b7463e54e67a7c2364eef3e2e3b88
Moroney L (FCES) <14075334@glam.ac.uk>
2017-03-14 18:58:20 +0000

    Fixed camera fow and plane position, camera now zooms by moving instead of changing fov so that the fow plane follows the main player camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -21,6 +21,10 @@ public class CameraController : MonoBehaviour {
     public float boundary = 50;
     public float speed = 5;
 
+    public Camera fowCamera;
+
+    private Vector3 oldPos;
+
     private void Awake()
     {
         me = GetComponent<Camera>();
@@ -33,6 +37,11 @@ public class CameraController : MonoBehaviour {
         screenHeight = Screen.height;
 
         Cursor.lockState = CursorLockMode.Confined;
+
+        me.fieldOfView = 74;
+        fowCamera.fieldOfView = 100;
+
+        oldPos = myPlayer.position;
     }
 
     public void SetPlayer(Transform _myPlayer)
@@ -42,10 +51,16 @@ public class CameraController : MonoBehaviour {
 
     private void Update ()
     {
+        /*
         float fov = me.fieldOfView;
         fov += Input.GetAxis("Mouse ScrollWheel") * -scrollSensitivity;
         fov = Mathf.Clamp(fov, minFov, maxFov);
         me.fieldOfView = fov;
+        */
+        if ((Vector3.Distance(transform.position, myPlayer.position) > 5 || Input.GetAxis("Mouse ScrollWheel") < 0)
+            && (Vector3.Distance(transform.position, myPlayer.position) < 50|| Input.GetAxis("Mouse ScrollWheel") > 0))
+            transform.position = Vector3.MoveTowards(transform.position, myPlayer.position, Input.GetAxis("Mouse ScrollWheel") * 5);
+        //Debug.Log(Input.GetAxis("Mouse ScrollWheel"));
 
         /*
         if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
@@ -55,39 +70,42 @@ public class CameraController : MonoBehaviour {
             transform.position = transform.position + translation;
         }
         */
-        /*
-        float x = 0;
-        float z = 0;
-        if (Input.mousePosition.x > screenWidth - boundary)
+        ///*
+        if (!cameraLocked)
         {
-            float multiplier = Input.mousePosition.x - screenWidth + boundary;
-            multiplier /= boundary;
-            if (multiplier > 1) multiplier = 1;
-            x += speed * Time.deltaTime * multiplier; // move on +X axis
+            float x = 0;
+            float z = 0;
+            if (Input.mousePosition.x > screenWidth - boundary)
+            {
+                float multiplier = Input.mousePosition.x - screenWidth + boundary;
+                multiplier /= boundary;
+                if (multiplier > 1) multiplier = 1;
+                x += speed * Time.deltaTime * multiplier; // move on +X axis
+            }
+            if (Input.mousePosition.x < 0 + boundary)
+            {
+                float multiplier = boundary - Input.mousePosition.x;
+                multiplier /= boundary;
+                if (multiplier > 1) multiplier = 1;
+                x -= speed * Time.deltaTime * multiplier; // move on -X axis
+            }
+            if (Input.mousePosition.y > screenHeight - boundary)
+            {
+                float multiplier = Input.mousePosition.y - screenHeight + boundary;
+                multiplier /= boundary;
+                if (multiplier > 1) multiplier = 1;
+                z += speed * Time.deltaTime * multiplier; // move on +Z axis
+            }
+            if (Input.mousePosition.y < 0 + boundary)
+            {
+                float multiplier = boundary - Input.mousePosition.y;
+                multiplier /= boundary;
+                if (multiplier > 1) multiplier = 1;
+                z -= speed * Time.deltaTime * multiplier; // move on -Z axis
+            }
+            transform.position = transform.position + new Vector3(x, 0, z);
         }
-        if (Input.mousePosition.x < 0 + boundary)
-        {
-            float multiplier = boundary - Input.mousePosition.x;
-            multiplier /= boundary;
-            if (multiplier > 1) multiplier = 1;
-            x -= speed * Time.deltaTime * multiplier; // move on -X axis
-        }
-        if (Input.mousePosition.y > screenHeight - boundary)
-        {
-            float multiplier = Input.mousePosition.y - screenHeight + boundary;
-            multiplier /= boundary;
-            if (multiplier > 1) multiplier = 1;
-            z += speed * Time.deltaTime * multiplier; // move on +Z axis
-        }
-        if (Input.mousePosition.y < 0 + boundary)
-        {
-            float multiplier = boundary - Input.mousePosition.y;
-            multiplier /= boundary;
-            if (multiplier > 1) multiplier = 1;
-            z -= speed * Time.deltaTime * multiplier; // move on -Z axis
-        }
-        transform.position = transform.position + new Vector3(x, 0, z);
-        */
+        //*/
         if (Input.GetKeyDown(KeyCode.Space))
         {
             cameraLocked = !cameraLocked;
@@ -95,7 +113,12 @@ public class CameraController : MonoBehaviour {
 
         if(cameraLocked && myPlayer)
         {
-            transform.position = initialPosition + myPlayer.position;
+            //transform.position = initialPosition + myPlayer.position;
+            if(oldPos != myPlayer.position)
+            {
+                transform.position += myPlayer.position - oldPos;
+                oldPos = myPlayer.position;
+            }
         }
         else
         {
