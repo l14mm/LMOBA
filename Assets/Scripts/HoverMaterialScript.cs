8cee18e0370f8d1fbfbd923a37c83cfd2e93ef54
liammoroney <liam.moroney14@gmail.com>
2017-04-14 16:25:13 +0100

    Brand new implementation of hover material/shader, allows each prefab/model to have its own outline colour and width, which can be changed at any time. Initial player camera position is closed to player

--- Assets/Scripts/HoverMaterialScript.cs
+++ Assets/Scripts/HoverMaterialScript.cs
@@ -7,15 +7,40 @@ using UnityEngine;
 public class HoverMaterialScript : NetworkBehaviour
 {
     public Material defaultMat;
-    public Material selectedMat;
+    public Material _selectedMat;
     public List<Renderer> models = new List<Renderer>();
+    private Material mySelectedMat;
+    public Color outlineColour = Color.red;
+    [Range(0.0f, 1.0f)]
+    public float outlineWidth = 0.33f;
+
+    void Start()
+    {
+        //if (!_selectedMat)
+            //return;
+
+        // Make a new material out of the reference so we don't change the default selected material for everthing
+        mySelectedMat = new Material(_selectedMat);
+
+        mySelectedMat.SetTexture("_MainTex", defaultMat.mainTexture);
+        mySelectedMat.SetColor("_OutlineColor", outlineColour);
+        mySelectedMat.SetFloat("_Outline", outlineWidth);
+        // = new Color(255, 0, 164)
+        //Select();
+    }
+
+    void Update2()
+    {
+        mySelectedMat.SetColor("_OutlineColor", outlineColour);
+        mySelectedMat.SetFloat("_Outline", outlineWidth);
+    }
 
     public void Select()
     {
         //GetComponent<Renderer>().material = selectedMat;
         for(int i = 0; i < models.Count; i++)
         {
-            models[i].material = selectedMat;
+            models[i].material = mySelectedMat;
         }
     }
 
